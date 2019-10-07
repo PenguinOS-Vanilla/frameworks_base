22f2acfe4a06ca81ce526e4d2c586eaedfe23579
darkobas <darkobas@gmail.com>
2019-10-07 20:31:48 +0200

    base: add option to enable AOD on charging only [1/2]
    idoybh edits: Adapted to A11's settings backup
    [jhonboy121]: adapt to A12 changes in DozeParameters. Also in the og commit DOZE_ALWAYS_ON was being read from System namespace instead of Secure, fixed it here. Make use of SettingsProxy utlity classes for settings / getting values
    
    Change-Id: I831583fde68de15788e3d7ecab55d864726d140f
    
    Commit message #2:
    
    base: check whether device is charging when alwaysOnChargingEnabled is called
    
    * Using settings for this is quite redundant. And if you toggle aod on charge after plugging in then aod won't turn on unless you unplug and plug again (since DOZE_ON_CHARGE_NOW is set only if aod on charge is enabled and device is plugged in"
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Commit message #3:
    
    base: fix deadlock between activity manager and power manager
    
    * Using the battery manager intent to query plugged in status was the root cause of deadlock,
      so inside power manager, user mIsPowered and setting value instead of using the intent based power status
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: adapted to A13
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I8a69ee3e5cdb3181dcc1c6126bfaa10cdf8440d7
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14495,6 +14495,12 @@ public final class Settings {
                 "otp_redaction_lock_time";
 
         /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";        
+
+        /**
          * boolean value. toggles swipe up hint in gestural nav mode
          * @hide
          */
