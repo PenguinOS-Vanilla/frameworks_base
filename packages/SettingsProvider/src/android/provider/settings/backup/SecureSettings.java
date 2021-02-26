3a581aab43ab358fc28d71c99d76bfc1353b2ad5
Ido Ben-Hur <idoybh2@gmail.com>
2021-02-27 01:52:33 +0200

    base: Allow scheduling always on display [1/2]
    From sunset to sunrise or at a custom time
    
    Change-Id: Ib76ecb1855ac215de6d7cd2f2346abfce6cc2214
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ==========================
     ==== Squashed commits ====
     ==========================
    
    AutoAODService: Add support for mixed time & sun modes [1/2]
    
    Also refactor some code
    
    Change-Id: I3a78dec88f532766d00e0d1d276c27dc9d3dc68f
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Account for disabled doze
    
    Change-Id: I30f52c4e3db2a27f9cde01662a25eee33e225414
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Slightly improve code and docs
    
    * Correctly link local vars and functions in docs
    * Make vars we can final
    * Don't use String.valueOf() where we don't have to
    * Should be Integer.parseInt() and not Integer.valueOf()
    
    Change-Id: I5892858c7142113ad3c9c87ddf00e58b18508207
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Improve some code
    
    And call using the right handler on settings change
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Use Calendar.add instead of Calendar.roll
    
    Roll doesn't always give the intended result which can cause an alarm to be set to the distance past/future on the end of the month
    Also make sure we cancel previous alarms before setting new ones, just incase
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Properly handle reboots & fix some logic
    
    Use shared preferences to find whether the user aborted current scheduled session
    In case that happens - don't automatically toggle AOD on boot until we passed the next alarm
    
    In addition:
    * Don't toggle AOD onTwilightStateChanged nor in mTimeChangedReceiver - they should just change the alarms
    * Properly re-init on ALL setting changes, not just mode
    * Never disable AOD on boot just because the setting is disabled
    * Never trigger doze / screen on when interactive
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    AutoAODService: Better check for doze enablement
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    
     ====
    
    Change-Id: I38bb54ee967eeb08c9b394efff26912e83bb6aa4
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -133,6 +133,8 @@ public class SecureSettings {
         Settings.Secure.POWER_MENU_LOCKED_SHOW_CONTENT,
         Settings.Secure.DOZE_ENABLED,
         Settings.Secure.DOZE_ALWAYS_ON,
+        Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE,
+        Settings.Secure.DOZE_ALWAYS_ON_AUTO_TIME,
         Settings.Secure.DOZE_ALWAYS_ON_INACTIVITY_DETECTION,
         Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED,
         Settings.Secure.DOZE_PICK_UP_GESTURE,
