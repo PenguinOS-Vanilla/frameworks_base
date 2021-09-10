5f5805bc04aa2a21bd1af7d1414bf6fe43cb6dfa
Adithya <gh0strider.2k18.reborn@gmail.com>
2021-09-10 14:20:54 +0530

    SystemUI: Show daily data usage in QS footer [SQUASHED]
     * Refactor existing build text view
     * Adapt to T
     * Improve and use controller
     * Hide when there is no wifi or sim
    
    Squashed:
     * SettingsLib: Add method to get daily data usage
     * SystemUI: Show wifi usage in QS footer when available
     * SystemUI: Show QS data usage suffix only when necessary
     * SystemUI: QS footer data usage improvements and cleanup
    
    @neobuddy89:
      * Drop QSFooterView changes as it is no longer used
      * Complete rewrite for QS new compose (A16)
      * Replicate earlier QSFooterView changes in BuildNumber
      * Debounce updates for smooth transitions
      * Handle edge cases of unknown ssids
      * Tested with wifi and sims
      * Update long click on text
      * Add tap to show weekly data usage
    
    Change-Id: Ifcd39167e6522935a8a29dc89d7e5f415893d266
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7338,6 +7338,18 @@ public final class Settings {
         public static final String LS_CLOCK_HIDE = "ls_clock_hide";
 
         /**
+         * Whether to show daily/weekly data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
+        /**
+         * Persist setting for showing either daily or weekly data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE_WINDOW = "qs_show_data_usage_window";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
