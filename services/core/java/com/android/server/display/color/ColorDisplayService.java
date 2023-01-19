959b5158aad756c56b055219b4b1865999c56699
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-19 15:01:15 +0200

    base: Allow disabling color inversion support [1/2]
    Via an overlay
    Some kernels won't support that no more
    Also make sure it's never enabled, even if the setting is set to true
    
    [joeyhuab] Adapt to A17
    
    Change-Id: I7b7c54ca5bd97012412ffa84ea5b4c705e7785c4

--- services/core/java/com/android/server/display/color/ColorDisplayService.java
+++ services/core/java/com/android/server/display/color/ColorDisplayService.java
@@ -359,8 +359,9 @@ public final class ColorDisplayService extends SystemService {
     }
 
     private boolean isColorInversionInSetupWizardEnabled() {
-        return getContext().getResources()
-               .getBoolean(R.bool.config_enableColorInversionInSetupWizard)
+        return isAccessibilityInversionAvailable()
+               && getContext().getResources()
+                      .getBoolean(R.bool.config_enableColorInversionInSetupWizard)
                && Flags.enableColorInversionInSuw();
     }
 
@@ -703,7 +704,7 @@ public final class ColorDisplayService extends SystemService {
     }
 
     private void setUpColorInversionAccessibility() {
-        if (mColorInversionObserverRegistered) {
+        if (mColorInversionObserverRegistered || !isAccessibilityInversionAvailable()) {
             return;
         }
         final ContentResolver cr = getContext().getContentResolver();
@@ -904,10 +905,16 @@ public final class ColorDisplayService extends SystemService {
     }
 
     private boolean isAccessiblityInversionEnabled() {
-        return Secure.getIntForUser(getContext().getContentResolver(),
+        return isAccessibilityInversionAvailable()
+            && Secure.getIntForUser(getContext().getContentResolver(),
             Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED,
             /* default= */ SETTINGS_DISABLED_VALUE,
             mCurrentUser) == SETTINGS_ENABLED_VALUE;
+    }
+
+    private boolean isAccessibilityInversionAvailable() {
+        return getContext().getResources().getBoolean(
+                R.bool.config_displayInversionAvailable);
     }
 
     private boolean isAccessibilityEnabled() {
