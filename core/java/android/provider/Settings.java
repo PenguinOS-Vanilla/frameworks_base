519eee8b7275087f2ccbc99ebfc127c25bd748ea
Oliver Scott <olivercscott@gmail.com>
2024-05-02 08:21:48 +0000

    Work profile work hours feature
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14215,6 +14215,12 @@ public final class Settings {
         public static final String HBM_SETTING_KEY =
                 "com.android.server.display.HBM_SETTING_KEY";
 
+	/**
+         * Custom time when user is scheduled to deactivate
+         * @hide
+         */
+        public static final String USER_ACTIVITY_END_TIME = "user_activity_end_time";
+
         /**
          * Whether to turn on black theme
          * @hide
