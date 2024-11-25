91c7f7c1150791126ca956073bef62ff18582d95
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-25 11:14:01 +0800

    services: Fix sleep mode service initialization
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -512,9 +512,6 @@ public final class SystemServer implements Dumpable {
             "/apex/com.android.uwb/javalib/service-ranging.jar";
     private static final String RANGING_SERVICE_CLASS = "com.android.server.ranging.RangingService";
 
-    private static final String SLEEP_MODE_SERVICE_CLASS =
-            "com.android.server.power.SleepModeService";
-
     private static final String TETHERING_CONNECTOR_CLASS = "android.net.ITetheringConnector";
 
     private static final String DEVICE_TO_DEVICE_SERVICE_CLASS =
@@ -3570,7 +3567,7 @@ public final class SystemServer implements Dumpable {
         t.traceEnd();
 
         t.traceBegin("SleepModeService");
-        mSystemServiceManager.startService(SLEEP_MODE_SERVICE_CLASS);
+        mSystemServiceManager.startService(SleepModeService.class.getName());
         t.traceEnd();
 
         t.traceBegin("HealthConnectManagerService");
