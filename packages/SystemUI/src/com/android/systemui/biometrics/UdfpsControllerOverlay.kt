feb56135b14dd0ba29e013336f9d0ab850a49853
pjgowtham <pjgowtham@gmail.com>
2025-06-22 11:00:08 +0530

    UdfpsHelper: Track brightness mirror to hide/show dim layer
    This provides a much deeper integration with UDFPS dim layer and the new
    brightness slider such that we don't need to look for APIs that can
    track temporary brightness. That is because when the brightness mirror
    is shown, there is no necessity for the UDFPS dim layer to be present.
    This change also isn't functional with the legacy slider.
    
    It also fixes the issue where the dim layer is hidden with QS expansion
    -> Device Control where the alternate bouncer is brought up.
    
    Test: Ensure that appropriate transparency of the dim layer is set when
    dragging or selecting the preferred brightness with the slider.
    
    Change-Id: I53f78cbcfd3b42a3b5dbe6110424687ebfac102d

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -57,6 +57,7 @@ import com.android.systemui.keyguard.shared.model.KeyguardState
 import com.android.systemui.power.domain.interactor.PowerInteractor
 import com.android.systemui.res.R
 import com.android.systemui.scene.domain.interactor.SceneInteractor
+import com.android.systemui.settings.brightness.domain.interactor.BrightnessMirrorShowingInteractor
 import com.android.systemui.shade.domain.interactor.ShadeInteractor
 import com.android.systemui.scene.shared.flag.SceneContainerFlag
 import com.android.systemui.scene.shared.model.Scenes
