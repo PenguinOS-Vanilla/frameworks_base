eaf313a4592eb916d2cc041522a9c0a3ebcd1c4a
NurKeinNeid <nurkeinneid@derpfest.org>
2025-10-02 00:56:19 +0200

    SystemUI: Allow to change system-wide blur intensity [1/2]
    Change-Id: I23a2fcb1ba2121328d32e346f693f8b8d086693e
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -824,6 +824,27 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         mColorExtractor.addOnColorsChangedListener(mOnColorsChangedListener);
 
+        Uri blurIntensity = Settings.System.getUriFor(Settings.System.BLUR_INTENSITY);
+        ContentObserver contentObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                if (uri.equals(blurIntensity)) {
+                    int newValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.BLUR_INTENSITY, 100, // 100% = system default
+                            UserHandle.USER_CURRENT);
+                    mContext.getMainExecutor().execute(() -> {
+                        com.android.systemui.statusbar.BlurUtils blurUtilsInstance =
+                                com.android.systemui.statusbar.BlurUtils.getBlurUtilsInstance();
+                        if (blurUtilsInstance != null) {
+                            blurUtilsInstance.setCustomBlurIntensity(newValue);
+                        }
+                    });
+                }
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                blurIntensity, false, contentObserver);
+        contentObserver.onChange(true, blurIntensity);
 
         mDisplayManager = mContext.getSystemService(DisplayManager.class);
 
