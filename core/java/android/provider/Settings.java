77e5345719d008aa0e5be32b23cbf6116d8f0d4a
Ghosuto <clash.raja10@gmail.com>
2025-10-25 12:56:26 +0000

    SystemUI: [squashed] Introduce wallpaper Lock glymps [1/2]
    Introduce Lock glymps
    
    Change-Id: Ib696e2ab7b05c1166ab1ed0677423796ab0dc3f3
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    
    SystemUI: Wallpaper glymps wallpaper preview with manual trigger
    
    Change-Id: I921aba8ef2dfbbfbe5f24da704087ea1ce9ad682
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    
    SystemUI: Add support for home screen wallpaper changes
    
    Change-Id: I2f980ad6494742146bfe46a0ed93202c0c233153
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    
    SystemUI: Introduce timer-based wallpaper change feature to Lock Glymps
    
    Change-Id: I83b0984866d2ad294d5fb4fd4a412de96785eae3
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7350,6 +7350,48 @@ public final class Settings {
         public static final String QS_SHOW_DATA_USAGE_WINDOW = "qs_show_data_usage_window";
 
         /**
+         * Lock Glymps - Dynamic lock screen wallpaper feature
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_ENABLED = "lock_glymps_enabled";
+
+        /**
+         * Lock Glymps wallpaper source (0=Random, 1=Custom URLs)
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_SOURCE = "lock_glymps_source";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_WIFI_ONLY = "lock_glymps_wifi_only";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_CACHE_SIZE = "lock_glymps_cache_size";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_CUSTOM_URLS = "lock_glymps_custom_urls";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_CHANGE_ON = "lock_glymps_change_on";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_TIMER_INTERVAL = "lock_glymps_timer_interval";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_WALLPAPER_TARGET = "lock_glymps_wallpaper_target";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
