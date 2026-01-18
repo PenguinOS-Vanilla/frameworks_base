f6d1acf8dcdef64536970a0d67aa459464018f3c
Pranav Vashi <neobuddy89@gmail.com>
2026-01-19 00:01:23 +0530

    SystemUI: Improve wifi/cell tiles
    * Make the WiFi tile actually disable WiFi instead of "pausing"
    * In the expanded state, when clicking on the secondary action
      change the popup dialog title from "Internet" to "Wi-Fi".
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I9911d675e3c3c6c0eedb0b9e11efdcbae4033e19
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -680,8 +680,8 @@ public class InternetDialogDelegateLegacy implements
         if (mMobileTitleText.getText().isEmpty()) {
             mMobileNetworkLayout.setVisibility(View.GONE);
         }
-        if (!internetContent.mHasActiveSubIdOnDds && (!internetContent.mIsWifiEnabled
-                || !internetContent.mIsCarrierNetworkActive)) {
+        if ((!internetContent.mHasActiveSubIdOnDds && (!internetContent.mIsWifiEnabled
+                || !internetContent.mIsCarrierNetworkActive)) || !mCanConfigMobileData) {
             mMobileNetworkLayout.setVisibility(View.GONE);
             if (mSecondaryMobileNetworkLayout != null) {
                 mSecondaryMobileNetworkLayout.setVisibility(View.GONE);
@@ -1045,7 +1045,7 @@ public class InternetDialogDelegateLegacy implements
     }
 
     CharSequence getDialogTitleText() {
-        return mInternetDetailsContentController.getDialogTitleText();
+        return mInternetDetailsContentController.getDialogTitleText(mCanConfigMobileData);
     }
 
     @Nullable
