01c7f79e9a24a81df24819717769c69ff0b07a5a
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-22 00:08:30 +0800

    Add support for game space
    Change-Id: Iaaf20bcbaa50c42dd6b7739ae233b245a2c5ad5c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- services/core/java/com/android/server/AxExtServiceFactory.java
+++ services/core/java/com/android/server/AxExtServiceFactory.java
@@ -21,6 +21,7 @@ import com.android.server.am.*;
 import com.android.server.pm.*;
 import com.android.server.spoof.AxSpoofManager;
 import com.android.server.spoof.IAxSpoofManager;
+import com.android.server.wm.GameSpaceService;
 import com.android.server.wm.WindowManagerService;
 
 public class AxExtServiceFactory {
@@ -83,6 +84,7 @@ public class AxExtServiceFactory {
     }
 
     public static void systemReady() {
+        GameSpaceService.systemReady();
         getSpoofManager().systemReady();
         IAxExtServiceFactory.initObscuraService();
     }
--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -1999,6 +1999,7 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
             }
             mBalController
                 .checkActivityAllowedToClearTask(task, callingUid, callingPid, callerActivityClassName);
+            GameSpaceService.get().removeTask(task, reason);    
         } finally {
             task.mInRemoveTask = false;
             mService.mChainTracker.endPartial();
