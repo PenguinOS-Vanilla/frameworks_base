0f4a59ee259ff5ef48a52ad1582f93c4ecf9d486
kenway214 <kenway214@outlook.com>
2026-06-28 22:24:48 +0530

    base: Implement Double tap to ambient [1/2]
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -338,6 +338,8 @@ public class SecureSettings {
         Settings.Secure.IDENTITY_CHECK_NOTIFICATION_VIEW_DETAILS_CLICKED,
         Settings.Secure.NOTIFICATION_BUNDLES_ALWAYS_EXPAND,
         Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_FOR_NOTIFICATIONS,
         Settings.Secure.ACCESSIBILITY_TOP_ROW_KEY_TARGETS,
         Settings.Secure.ACCESSIBILITY_QUICK_ACCESS_TARGETS,
