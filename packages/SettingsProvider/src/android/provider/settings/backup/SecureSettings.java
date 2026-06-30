ea55b235db56686e4b154664ac77a51fd5fdc9b2
kenway214 <kenway214@outlook.com>
2026-06-30 09:02:49 +0530

    SystemUI: Add toggle to control visibility of Bluetooth battery level in status bar
    [kenway214] Adaptation and cleanup for A17
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -266,6 +266,7 @@ public class SecureSettings {
         Settings.Secure.LOCK_SCREEN_SHOW_QR_CODE_SCANNER,
         Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
+        Settings.Secure.STATUS_BAR_BLUETOOTH_BATTERY,
         Settings.Secure.WEAR_TALKBACK_ENABLED,
         Settings.Secure.HBM_SETTING_KEY,
         Settings.Secure.ASSIST_TOUCH_GESTURE_ENABLED,
