4b0a93c31f963f105acf7a1d7879639bb032287c
rituj <ritujbeniwal@gmail.com>
2026-07-14 20:51:09 +0530

    base: Add vibration patterns from OOS [1/3]
    Change-Id: I9622c17a1969598af71bcee573a576b752b96c5e
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6145,6 +6145,19 @@ public final class Settings {
             "custom_notification_vibration_pattern";
 
         /**
+         * Which Vibration Pattern to use
+         * 0: dzzz-dzzz
+         * 1: dzzz-da
+         * 2: mm-mm-mm
+         * 3: da-da-dzzz
+         * 4: da-dzzz-da
+         * @hide
+         */
+        @Readable
+        public static final String RINGTONE_VIBRATION_PATTERN =
+            "ringtone_vibration_pattern";
+
+        /**
          * Persistent store for the system-wide default alarm alert.
          *
          * @see #RINGTONE
