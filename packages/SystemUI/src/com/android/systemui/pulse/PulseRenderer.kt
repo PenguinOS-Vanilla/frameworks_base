66d369ec81eccaf61e2023ff96a8a5b78285c6ca
Pranav Vashi <neobuddy89@gmail.com>
2026-09-19 01:07:01 +0530

    SystemUI: Pulse: Add particle and waveform renderer styles [1/2]
    * Based on sparkle and minimal wave styles.
    
    Change-Id: Id9d16844393b2982c65374ed600a483012b275a7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/pulse/PulseRenderer.kt
+++ packages/SystemUI/src/com/android/systemui/pulse/PulseRenderer.kt
@@ -71,6 +71,8 @@ class PulseRenderer(
             "sparkle" -> style !is SparkleStyleRenderer
             "matrix" -> style !is MatrixStyleRenderer
             "dotwave" -> style !is DotWaveStyleRenderer
+            "particle" -> style !is ParticleStyleRenderer
+            "waveform" -> style !is WaveformStyleRenderer
             else -> false
         }
 
@@ -94,6 +96,8 @@ class PulseRenderer(
             "sparkle" -> SparkleStyleRenderer(settingsRepo)
             "matrix" -> MatrixStyleRenderer(settingsRepo)
             "dotwave" -> DotWaveStyleRenderer(settingsRepo)
+            "particle" -> ParticleStyleRenderer(settingsRepo)
+            "waveform" -> WaveformStyleRenderer(settingsRepo)
             else -> SolidLineStyleRenderer(settingsRepo)
         }
     }
